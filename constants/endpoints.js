ae6b2f1fffd5eafe69e2631b7520581232a33c6a
matteo <matte.rubbiani@gmail.com>
2021-05-11 15:02:56 +0200

    H

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -1,6 +1,7 @@
 Endpoints = {
-    JOIN_GAME: "join-game",
+    CONNECT_TO_GAME: "connect-to-game", // -> status a me + lobby modified se status 0
     LOBBY_MODIFIED: "lobby-modified", //{players: [], currentPlayer: 0, totalTurns: 5, admin: 0}
+    JOIN_GAME: "join-game",
 }
 
 module.exports = Endpoints
--- models/Game.js
+++ models/Game.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
-
 mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 const GameSchema = new mongoose.Schema({
     id: {
