92c17a7f2130f4be08e5e6b2cab9610167f5fb23
matteo <matte.rubbiani@gmail.com>
2021-05-12 16:45:58 +0200

    H

--- routes/game.js
+++ routes/game.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {makeId} = require("../constants/constants")
+const Games = require("../managers/activeGames")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get('/getNewId', async function(req, res, next) {
+    let id = makeId()
+    while (await Games.getActiveGameById(id)){
+        id = makeId()
+    }
+    res.send(id);
 });
 
 module.exports = router;
