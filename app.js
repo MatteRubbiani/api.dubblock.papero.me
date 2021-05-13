5d2762c1cb77d87079f887ee760febee27a89212
matteo <matte.rubbiani@gmail.com>
2021-05-13 09:23:12 +0200

    J

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ io.on('connection', socket => {
             game.addPlayer(userId, username)
             await game.saveToDb()
         }
+        console.log(game.status)
         if (game.status === 0) {
             console.log(game.getGame(userId))
             await sendLobbyChangedToPlayers(game)
